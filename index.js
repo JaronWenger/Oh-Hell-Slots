7457bbd86b9dde356d547630aba68cc49adead45
Jaron Wenger <147181586+JaronWenger@users.noreply.github.com>
2024-02-03 19:50:03 -0700

    2nd sim complete

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ if (playerOnesTurn && trump){
 
 var winPercentageArray = [];
 const trumpCardValueArray = ["2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks", "As"];
-const alphaCardValueArray = ["2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks", "As"];
+const alphaCardValueArray = ["2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd", "Ad"];
 
 
 function sim() {
